9203788e8a577c66ed086a3d54dd46cfb35151f1
LizardonPT <joaofilipegoncalves2000@gmail.com>
2020-01-16 17:43:56 +0000

    Revela as cores, next step, ver se são iguais

--- gameplay.py
+++ gameplay.py
@@ -1,42 +1,76 @@
 import pygame
+import pygame.freetype
+import random
+ 
+
 pygame.init()
 res = (1440, 740)
 screen = pygame.display.set_mode(res)
+image = pygame.image.load("shuffle.png")
+my_font = pygame.freetype.Font("NotoSans-Regular.ttf", 24)
 yellow = (255,255,0)
+white = (255,255,255)
 green = (0, 200, 0)
-x = 260
-y = 60
-n = 0
-screen.fill((0,0,20))
-square_y = (screen, yellow, (x, y, 50, 50), 0)
-square_g = (screen, green, (x, y, 50, 50), 0)
-figures = [square_y, square_g]
+score = 0
+cores = [(200,0,0), (200,200,200), (0,0,200), (200,200,0), (0,200,200), (150,50,200)]
 
-while(True):
-    for event in pygame.event.get():
-        if(event.type == pygame.QUIT):
-            exit()
-    
-    
-    #cards = []
-    
-    if (n <12):
+
+def lvl1():
+    x = 420
+    y = 60
+    i = 0
+    baralhar = [0,0,1,1,2,2,3,3,4,4,5,5]
+
+    screen.fill((0,0,20))
+    random.shuffle(baralhar)
+
+    #desenha as cartas
+    cardpos = []
+
+    cardpos.append(pygame.Rect(420, 60, 120, 180))
+    for a in range(12):
+        pygame.draw.rect(screen, green, (x, y, 120, 180), 0)
         x = x + 160
-        pygame.draw.rect(screen, green, (x, y, 120, 180), 0)
-        n = n + 1
-        #cards.append(x)
-        #cards.append(y)
+        
+        if (a == 3 or a == 7):
+            y = y + 220
+            x = 260
+            x = x + 160
 
-    if (n ==4 or n == 8):
-        y = y + 220
-        x = 420
-        n = n + 1
-        pygame.draw.rect(screen, green, (x, y, 120, 180), 0)
-        #cards.append(x)
-        #cards.append(y)
-    pos = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    if (420<pos[0]<420+120 and 60<pos[1]<60+180 and click[0]):
-        pygame.draw.rect(screen, yellow, (460,120,40,40), 0)
+        m_rect = pygame.Rect(x,y, 120, 180)
+        cardpos.append(m_rect)
+ 
+    while(True):
+        for event in pygame.event.get():
+            if(event.type == pygame.QUIT):
+                exit()
+            pos = pygame.mouse.get_pos()
+            for card in cardpos:
+                if card.collidepoint(pos) and i<12:
+                    if(event.type == pygame.MOUSEBUTTONUP):
+                        cardpos.remove(card)
+                        pygame.draw.rect(screen, cores[baralhar[i]], card, 0)
+                        i+=1
+                        
+    
+        #desenha botão exit
+        pygame.draw.rect(screen, yellow, (50,650,140,30), 2)
+        my_font.render_to(screen, (100,655), "Exit", yellow)
+        #desenha o score
+        my_font.render_to(screen, (50,50), "Score: " + str(score), yellow)
+        pos = pygame.mouse.get_pos()
+        #coisas brancaas?
 
-    pygame.display.flip()
+        #botão exit    
+        if(50<pos[0]<50+140 and 650<pos[1]<650+30):
+            pygame.draw.rect(screen, white, (50,650,140,30), 2)
+            my_font.render_to(screen, (100,655), "Exit", white)
+       
+        pygame.display.flip()
+        
+        #voltar ao menu
+        if(50<pos[0]<50+140 and 650<pos[1]<650+30):
+            if(event.type == pygame.MOUSEBUTTONUP):
+                exit()
+                
+lvl1()
--- projeto.py
+++ projeto.py
@@ -2,7 +2,7 @@ import pygame
 import pygame.freetype
 import random
  
- 
+
 pygame.init()
 res = (1440, 740)
 screen = pygame.display.set_mode(res)
@@ -13,95 +13,55 @@ white = (255,255,255)
 green = (0, 200, 0)
 score = 0
 cores = [(200,0,0), (200,200,200), (0,0,200), (200,200,0), (0,200,200), (150,50,200)]
-baralhar = [0,0,1,1,2,2,3,3,4,4,5,5]
+
 
 def lvl1():
-    cartas = [420,60]
     x = 420
     y = 60
-    m = 0
-    z = 0
     i = 0
+    baralhar = [0,0,1,1,2,2,3,3,4,4,5,5]
 
     screen.fill((0,0,20))
     random.shuffle(baralhar)
-    print(baralhar)
 
     #desenha as cartas
+    cardpos = []
+
+    cardpos.append(pygame.Rect(420, 60, 120, 180))
     for a in range(12):
         pygame.draw.rect(screen, green, (x, y, 120, 180), 0)
         x = x + 160
+        
         if (a == 3 or a == 7):
             y = y + 220
             x = 260
             x = x + 160
+
+        m_rect = pygame.Rect(x,y, 120, 180)
+        cardpos.append(m_rect)
  
     while(True):
         for event in pygame.event.get():
             if(event.type == pygame.QUIT):
                 exit()
- 
+            pos = pygame.mouse.get_pos()
+            for card in cardpos:
+                if card.collidepoint(pos) and i<12:
+                    if(event.type == pygame.MOUSEBUTTONUP):
+                        cardpos.remove(card)
+                        pygame.draw.rect(screen, cores[baralhar[i]], card, 0)
+                        i+=1
+                        
+    
         #desenha botão exit
         pygame.draw.rect(screen, yellow, (50,650,140,30), 2)
         my_font.render_to(screen, (100,655), "Exit", yellow)
         #desenha o score
         my_font.render_to(screen, (50,50), "Score: " + str(score), yellow)
-        #muda as cores de amarelo para branco
         pos = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
         #coisas brancaas?
-        
-        #revela a carta
-        
-        for n in range(12):
-            if(cartas[0]+z*160<pos[0]<cartas[0]+z*160+120 and cartas[1]+m*220<pos[1]<cartas[1]+m*220+180 and click[0]):
-                pygame.draw.rect(screen, cores[baralhar[i]], (cartas[0]+z*160,cartas[1]+m*220,120,180), 0)
-                z = z + 1
-                i = i + 1
-            if(n==3 and n==7):
-                z = 0
-                m = m + 1
 
-        
-
-        """if(420<pos[0]<420+120 and 60<pos[1]<60+180 and click[0]):
-            pygame.draw.rect(screen, cores[baralhar[0]], (420,60,120,180), 0)
-        
-        if(580<pos[0]<580+120 and 60<pos[1]<60+180 and click[0]):
-            pygame.draw.rect(screen, cores[baralhar[1]], (580,60,120,180), 0)
-
-        if(740<pos[0]<740+120 and 60<pos[1]<60+180 and click[0]):
-            pygame.draw.rect(screen, cores[baralhar[2]], (740,60,120,180), 0)
-
-        if(900<pos[0]<900+120 and 60<pos[1]<60+180 and click[0]):
-            pygame.draw.rect(screen, cores[baralhar[3]], (900,60,120,180), 0)
-
-        if(420<pos[0]<420+120 and 280<pos[1]<280+180 and click[0]):
-            pygame.draw.rect(screen, cores[baralhar[4]], (420,280,120,180), 0)
-        
-        if(580<pos[0]<580+120 and 280<pos[1]<280+180 and click[0]):
-            pygame.draw.rect(screen, cores[baralhar[5]], (580,280,120,180), 0)
-
-        if(740<pos[0]<740+120 and 280<pos[1]<280+180 and click[0]):
-            pygame.draw.rect(screen, cores[baralhar[6]], (740,280,120,180), 0)
-
-        if(900<pos[0]<900+120 and 280<pos[1]<280+180 and click[0]):
-            pygame.draw.rect(screen, cores[baralhar[7]], (900,280,120,180), 0)
-
-        if(420<pos[0]<420+120 and 500<pos[1]<500+180 and click[0]):
-            pygame.draw.rect(screen, cores[baralhar[8]], (420,500,120,180), 0)
-        
-        if(580<pos[0]<580+120 and 500<pos[1]<500+180 and click[0]):
-            pygame.draw.rect(screen, cores[baralhar[9]], (580,500,120,180), 0)
-
-        if(740<pos[0]<740+120 and 500<pos[1]<500+180 and click[0]):
-            pygame.draw.rect(screen, cores[baralhar[10]], (740,500,120,180), 0)
-
-        if(900<pos[0]<900+120 and 500<pos[1]<500+180 and click[0]):
-            pygame.draw.rect(screen, cores[baralhar[11]], (900,500,120,180), 0)"""
-        
-                
- 
+        #botão exit    
         if(50<pos[0]<50+140 and 650<pos[1]<650+30):
             pygame.draw.rect(screen, white, (50,650,140,30), 2)
             my_font.render_to(screen, (100,655), "Exit", white)
@@ -109,8 +69,9 @@ def lvl1():
         pygame.display.flip()
         
         #voltar ao menu
-        if(50<pos[0]<50+140 and 650<pos[1]<650+30 and click[0]):
-            main()
+        if(50<pos[0]<50+140 and 650<pos[1]<650+30):
+            if(event.type == pygame.MOUSEBUTTONUP):
+                main()
  
 def lvl2():
     screen.fill((0,0,20))
@@ -130,10 +91,10 @@ def lvl2():
             my_font.render_to(screen, (100,655), "Exit", white)
        
         pygame.display.flip()
-        click = pygame.mouse.get_pressed()
         #voltar ao menu
-        if(50<pos[0]<50+140 and 650<pos[1]<650+30 and click[0]):
-            main()
+        if(50<pos[0]<50+140 and 650<pos[1]<650+30):
+            if(event.type == pygame.MOUSEBUTTONUP):
+                main()
  
 def main():
     while(True):
@@ -179,14 +140,16 @@ def main():
             my_font.render_to(screen, (700,370+185), "Exit", white)
         pygame.display.flip()
         #sair do jogo
-        click = pygame.mouse.get_pressed()
-        if(650<pos[0]<650+140 and 370+180<pos[1]<370+180+30 and click[0]):
-            exit()
+        if(650<pos[0]<650+140 and 370+180<pos[1]<370+180+30):
+            if(event.type == pygame.MOUSEBUTTONUP):
+                exit()
         #Nivel 1
-        if(650<pos[0]<650+140 and 370-40<pos[1]<370-40+30 and click[0]):
-            lvl1()
+        if(650<pos[0]<650+140 and 370-40<pos[1]<370-40+30):
+            if(event.type == pygame.MOUSEBUTTONUP):
+                lvl1()
         #Nivel 2
-        if(650<pos[0]<650+140 and 370<pos[1]<370+30 and click[0]):
-            lvl2()
+        if(650<pos[0]<650+140 and 370<pos[1]<370+30):
+            if(event.type == pygame.MOUSEBUTTONUP):
+                lvl2()
         pygame.time.wait(5)
 main()
